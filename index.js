39f969b4c7a99040c1a92ecff04fe2b76f34ad8a
navire <ErivanPereira>
2021-03-24 10:46:10 -0300

    a

--- index.js
+++
@@ -1,5 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { Pytorch } = NativeModules;
-
-export default Pytorch;
--- ios/Pytorch.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface Pytorch : NSObject <RCTBridgeModule>
-
-@end
